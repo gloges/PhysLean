19e1077e6477234e1a58ba88c931dcc3ddbab9b3
gloges <gerg1995@gmail.com>
2026-02-05 00:43:46 +0900

    feat: various posiont / momentum commutators

--- PhysLean/QuantumMechanics/DDimensions/Operators/Commutation.lean
+++ PhysLean/QuantumMechanics/DDimensions/Operators/Commutation.lean
@@ -15,6 +15,44 @@ noncomputable section
 open Constants
 open SchwartzMap ContinuousLinearMap
 
+-- TODO: Is there a mathlib way to do this?
+def kroneckerDelta {d : ℕ} (i j : Fin d) : ℝ := (if i = j then 1 else 0)
+
+local macro "δ[" i:term "," j:term "]" : term => `(kroneckerDelta $i $j)
+
+lemma kroneckerDelta_symm {d : ℕ} : ∀ (i j : Fin d), δ[i,j] = δ[j,i] := by
+  unfold kroneckerDelta
+  intro i j
+  refine ite_cond_congr ?_
+  exact Eq.propIntro Eq.symm Eq.symm
+
+lemma sum_kroneckerDelta [AddCommMonoid M] [Module ℝ M] {d : ℕ} :
+    ∀ (f : Fin d → M) (j : Fin d), (∑ (i : Fin d), δ[i,j] • f i = f j) := by
+  unfold kroneckerDelta
+  intro f j
+  simp only [ite_smul, one_smul, zero_smul, Finset.sum_ite_eq', Finset.mem_univ, ↓reduceIte]
+
+lemma sum_kroneckerDelta' [AddCommMonoid M] [Module ℝ M] {d : ℕ} :
+    ∀ (f : Fin d → M) (i : Fin d), (∑ (j : Fin d), δ[i,j] • f j = f i) := by
+  unfold kroneckerDelta
+  intro f i
+  simp only [ite_smul, one_smul, zero_smul, Finset.sum_ite_eq, Finset.mem_univ, ↓reduceIte]
+
+example (A : 𝓢(Space d, ℂ) →L[ℂ] 𝓢(Space d, ℂ)) : ⁅A, A⁆ = 0 := lie_self _
+example (A B : 𝓢(Space d, ℂ) →L[ℂ] 𝓢(Space d, ℂ)) : -⁅B, A⁆ = ⁅A, B⁆ := lie_skew _ _
+example (A B C : 𝓢(Space d, ℂ) →L[ℂ] 𝓢(Space d, ℂ)) :
+  ⁅A, ⁅B, C⁆⁆ + ⁅B, ⁅C, A⁆⁆ + ⁅C, ⁅A, B⁆⁆ = 0 := lie_jacobi _ _ _
+
+lemma commutator_leibniz_left {d : ℕ} (A B C : 𝓢(Space d, ℂ) →L[ℂ] 𝓢(Space d, ℂ)) :
+    ⁅A ∘L B, C⁆ = A ∘L ⁅B, C⁆ + ⁅A, C⁆ ∘L B := by
+  dsimp only [Bracket.bracket]
+  simp only [ContinuousLinearMap.mul_def, comp_assoc, comp_sub, sub_comp, sub_add_sub_cancel]
+
+lemma commutator_leibniz_right {d : ℕ} (A B C : 𝓢(Space d, ℂ) →L[ℂ] 𝓢(Space d, ℂ)) :
+    ⁅A, B ∘L C⁆ = B ∘L ⁅A, C⁆ + ⁅A, B⁆ ∘L C := by
+  dsimp only [Bracket.bracket]
+  simp only [ContinuousLinearMap.mul_def, comp_assoc, comp_sub, sub_comp, sub_add_sub_cancel']
+
 /-
 ## Position / position commutators
 -/
@@ -67,10 +105,63 @@ lemma momentumSqr_commutation_momentum {d : ℕ} (i : Fin d) : 𝐩² ∘L 𝐩[
 -/
 
 /-- `[xᵢ, pⱼ] = iℏ δᵢⱼ𝟙` -/
-@[sorryful]
 lemma position_commutation_momentum {d : ℕ} (i j : Fin d) : ⁅𝐱[i], 𝐩[j]⁆ =
-    (Complex.I * ℏ) • (if i = j then 1 else 0) • ContinuousLinearMap.id ℂ 𝓢(Space d, ℂ) := by
-  sorry
+    (Complex.I * ℏ * δ[i,j]) • ContinuousLinearMap.id ℂ 𝓢(Space d, ℂ) := by
+  dsimp only [Bracket.bracket]
+  ext ψ x
+  simp only [ContinuousLinearMap.smul_apply, SchwartzMap.smul_apply, coe_id', id_eq, smul_eq_mul,
+    coe_sub', coe_mul, Pi.sub_apply, Function.comp_apply, SchwartzMap.sub_apply]
+  rw [positionOperator_apply, momentumOperator_apply_fun]
+  rw [momentumOperator_apply, positionOperator_apply_fun]
+  simp only [neg_mul, Pi.smul_apply, smul_eq_mul, mul_neg, sub_neg_eq_add]
+
+  have h : (fun x ↦ ↑(x i) * ψ x) = (fun (x : Space d) ↦ x i) • ψ := rfl
+  rw [h]
+  rw [Space.deriv_smul (by fun_prop) (SchwartzMap.differentiableAt ψ)]
+  rw [Space.deriv_component]
+  rw [kroneckerDelta_symm, kroneckerDelta]
+  simp only [Complex.real_smul]
+  ring
+
+lemma position_position_commutation_momentum {d : ℕ} (i j k : Fin d) : ⁅𝐱[i] ∘L 𝐱[j], 𝐩[k]⁆ =
+    (Complex.I * ℏ * δ[i,k]) • 𝐱[j] + (Complex.I * ℏ * δ[j,k]) • 𝐱[i] := by
+  rw [commutator_leibniz_left]
+  rw [position_commutation_momentum, position_commutation_momentum]
+  ext ψ x
+  simp only [comp_smulₛₗ, RingHom.id_apply, comp_id, smul_comp, id_comp,
+    ContinuousLinearMap.add_apply, coe_smul', Pi.smul_apply, SchwartzMap.add_apply,
+    SchwartzMap.smul_apply, smul_eq_mul]
+  ring
+
+lemma position_commutation_momentum_momentum {d : ℕ} (i j k : Fin d) : ⁅𝐱[i], 𝐩[j] ∘L 𝐩[k]⁆ =
+    (Complex.I * ℏ * δ[i,k]) • 𝐩[j] + (Complex.I * ℏ * δ[i,j]) • 𝐩[k] := by
+  rw [commutator_leibniz_right]
+  rw [position_commutation_momentum, position_commutation_momentum]
+  ext ψ x
+  simp only [comp_smulₛₗ, RingHom.id_apply, comp_id, smul_comp, id_comp,
+    ContinuousLinearMap.add_apply, coe_smul', Pi.smul_apply, SchwartzMap.add_apply,
+    SchwartzMap.smul_apply, smul_eq_mul]
+
+lemma position_commutation_momentumSqr {d : ℕ} (i : Fin d) : ⁅𝐱[i], 𝐩²⁆ =
+    (2 * Complex.I * ℏ) • 𝐩[i] := by
+  unfold momentumOperatorSqr
+  rw [lie_sum]
+  conv_lhs =>
+    enter [2, i_1]
+    rw [commutator_leibniz_right]
+    rw [position_commutation_momentum]
+    simp only [comp_smulₛₗ, RingHom.id_apply, comp_smul, comp_id, smul_comp, id_comp]
+    calc
+      _ = (2 * Complex.I * ℏ * δ[i,i_1]) • 𝐩[i_1] := by
+        ext ψ x
+        simp only [ContinuousLinearMap.add_apply, coe_smul', Pi.smul_apply, SchwartzMap.add_apply,
+          SchwartzMap.smul_apply, smul_eq_mul]
+        ring
+      _ = δ[i,i_1] • ((2 * Complex.I * ℏ) • 𝐩[i_1]) := by
+        ext ψ x
+        simp only [coe_smul', Pi.smul_apply, SchwartzMap.smul_apply, smul_eq_mul, Complex.real_smul]
+        ring
+  rw [sum_kroneckerDelta']
 
 /-
 ## Angular momentum / position commutators
